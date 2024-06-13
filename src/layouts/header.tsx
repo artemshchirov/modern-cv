499d2591b2cf3177c59dd99e05fb0ec912a3ebb7
Artem Shchirov <artemshchirov@gmail.com>
2024-06-13 13:39:37 +0300

    fix(web): redeploy

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,20 +1,27 @@
-import { DialogTrigger } from '@radix-ui/react-dialog';
-
 import ContactDialog from '@/components/contact-dialog';
 import MobileMenu from '@/components/mobile-menu';
 import Status from '@/components/status';
 import ThemeSwitch from '@/components/theme-switch';
+import { Button } from '@/components/ui/button';
+import { DialogTrigger } from '@/components/ui/dialog';
 
 import EditIcon from '~/svg/edit.svg';
 
 export default function Header() {
   return (
     <header className='header flex justify-between items-center pb-2'>
-      <div>
+      <div className='flex gap-x-[18px]'>
         <Status />
+        <ContactDialog>
+          <DialogTrigger asChild>
+            <Button className='w-full dark:shadow-cv-btn-outer'>
+              Contact me
+            </Button>
+          </DialogTrigger>
+        </ContactDialog>
       </div>
       <div className='flex gap-x-4'>
-        <ThemeSwitch className='hidden md:flex' />
+        <ThemeSwitch />
         <ContactDialog>
           <DialogTrigger>
             <EditIcon className='w-[26px] h-[26px] md:hidden stroke-[#29303E] dark:stroke-white' />
